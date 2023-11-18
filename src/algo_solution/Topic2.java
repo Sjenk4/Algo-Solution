4fed267cd7d88129059670052f3717af157bdbd2
Ginnd7 <147168433+Ginnd7@users.noreply.github.com>
2023-11-18 22:14:42 +0700

    update

--- src/algo_solution/Topic2.java
+++ src/algo_solution/Topic2.java
@@ -2,8 +2,12 @@ package algo_solution;
 
 public class Topic2 {
 	public static void main(String[] args) {
-		int result = solution("bbbabb");
-		System.out.println("result :" + result);
+		String a = "babaa";
+		String b = "bbbab";
+		String c = "bbbaaabbb";
+		System.out.println("result a:" + solution(a));
+		System.out.println("result a:" + solution(b));
+		System.out.println("result a:" + solution(c));
 	}
 
 	public static int solution(String S) {
@@ -11,20 +15,20 @@ public class Topic2 {
 		int countDuplicateTemporary = 0;
 		int countDuplicateFinal = 0;
 		int lengthBlock = 0;
-		char[] charArray = S.toCharArray();
-		for (int i = 0; i < charArray.length - 1; i++) {
-			if (charArray[i] == charArray[i + 1]) {
+	;
+		for (int i = 0; i < S.length() - 1; i++) {
+			if (S.charAt(i) == S.charAt(i + 1)) {
 				countDuplicateTemporary++;
 			}
 			if (countDuplicateTemporary > countDuplicateFinal) {
 				countDuplicateFinal = countDuplicateTemporary;
 			}
-			if (charArray[i] != charArray[i + 1]) {
+			if (S.charAt(i) != S.charAt(i + 1)) {
 				lengthBlock++;
 				countDuplicateTemporary = 0;
 			}
 		}
-		result = (lengthBlock + 1) * (countDuplicateFinal + 1) - charArray.length;
+		result = (lengthBlock + 1) * (countDuplicateFinal + 1) - S.length();
 		return result;
 	}
 }
